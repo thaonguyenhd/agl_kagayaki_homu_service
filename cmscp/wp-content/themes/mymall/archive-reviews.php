69f607ce9362fd5841e5795b9fb685a80d38a338
mymall-allgrow <allgrow@mymall.co.jp>
2024-02-27 09:21:14 +0700

    Update archive-reviews.php

--- cmscp/wp-content/themes/mymall/archive-reviews.php
+++ cmscp/wp-content/themes/mymall/archive-reviews.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main>
 	<div class="page-tit-01 uk-flex uk-flex-wrap uk-child-width-1-2@m">
-		<figure class="img"><img src="<?= get_theme_file_uri(''); ?>/img/information/kv.jpg" alt=""></figure>
+		<figure class="img"><img src="<?= get_theme_file_uri(''); ?>/img/debts/kv.jpg" alt=""></figure>
 		<div class="detail uk-container">
 			<h1 class="tit">クチコミ一覧</h1>
 			<span class="text">Reviews</span>
@@ -10,35 +10,49 @@
 	<div id="info-01" class="com-sec-01">
 		<div class="uk-container">
 			<h2 class="com-tit-03">お客様によるクチコミをご紹介</h2>
-<?php if ( have_posts() ): ?>
+			<?php if ( have_posts() ): ?>
 			<ul class="uk-child-width-1-2@s uk-grid-match com-mt-l uk-grid" uk-grid>
-<?php while ( have_posts() ): the_post(); ?>
+			<?php while ( have_posts() ): the_post(); ?>
 				<li>
-					<?php $infor = get_field('infor'); ?>
-					<div class="inheritance-panel-01 reviews-panel-01">
-						<h4 class="type"><?= $infor['type-service'] ?></h4>
-						<div class="head uk-grid-small uk-flex-middle uk-flex-between" uk-grid>
+					<div class="inheritance-panel-01 review-panel-01">
+						<div class="head uk-grid-medium" uk-grid>
+							<div>
+								<img src="<?= get_theme_file_uri() ?>/img/common/ico_user.svg" alt="" width="90" class="image">
+							</div>
 							<div>
 								<div class="form-range-01">
-									<input type="range" value="<?= $infor['rating'] ?>" min="1" max="5" disabled>
+									<input type="range" value="<?= get_field('reviews_rating')??1 ?>" min="1" max="5" disabled>
 								</div>
-							</div>
-							<div>
-								<time class="date" datetime="<?= get_the_date('Y.m.d') ?>"><?= get_the_date('Y.m.d') ?></time>
+								<ul class="infor uk-grid-small" uk-grid>
+									<li>相談者年齢：<?= get_field('reviews_age') ?></li>
+									<li>依頼地域：<?= get_field('reviews_pref') ?></li>
+								</ul>
 							</div>
 						</div>
-						<ul class="infor">
-							<li class="prefectures"><?= $infor['prefectures'] ?></li>
-							<li class="age"><?= $infor['age'] ?></li>
+						<?php if(!empty(get_field('reviews_type'))): ?>
+						<ul class="type uk-flex uk-flex-wrap">
+							<?php foreach(get_field('reviews_type') as $i): ?>
+							<li>
+								<span>相続手続</span>
+							</li>
+							<?php endforeach; ?>
 						</ul>
-						<p><?= the_content() ?></p>
+						<?php endif; ?>
+						<p><?php the_content() ?></p>
+						<div class="uk-text-right mt10">
+							<time datetime="<?= get_the_date('Y/m/d') ?>" class="date">
+								<?= get_the_date('Y年n月j日掲載') ?>
+								<!-- 2024年2月15日掲載 -->
+							</time>
+						</div>
 					</div>
 				</li>
-<?php endwhile; ?>
+			<?php endwhile; ?>
 			</ul>
 			<?php get_template_part( 'parts/pagination' ); ?>
-<?php endif; ?>
+			<?php endif; ?>
 		</div>
 	</div><!-- /#info-01 -->
+	<?php get_template_part( 'parts/case-part' ); ?>
 </main>
 <?php get_footer(); ?>
