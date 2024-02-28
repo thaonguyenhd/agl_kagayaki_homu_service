15c00a2896f90566da0e8ba9d91dc04a965eb39d
mymall-allgrow <allgrow@mymall.co.jp>
2024-02-28 16:11:00 +0700

    Unify the indent space

--- cmscp/wp-content/themes/mymall/pages/review-form.php
+++ cmscp/wp-content/themes/mymall/pages/review-form.php
@@ -1,14 +1,14 @@
 <div class="page-tit-01 uk-flex uk-flex-wrap uk-child-width-1-2@m">
-    <figure class="img"><img src="<?= get_theme_file_uri(); ?>/img/contact/kv.jpg" alt=""></figure>
-    <div class="detail uk-container">
-        <h1 class="tit">クチコミ投稿フォーム</h1>
-        <span class="text">Review Form</span>
-    </div>
+	<figure class="img"><img src="<?= get_theme_file_uri(); ?>/img/contact/kv.jpg" alt=""></figure>
+	<div class="detail uk-container">
+		<h1 class="tit">クチコミ投稿フォーム</h1>
+		<span class="text">Review Form</span>
+	</div>
 </div>
 <section id="contact-03" class="com-sec-01">
-    <div class="uk-container">
-        <p>下記フォームに必要項目を入力し送信ボタンを押してください。<br>内容を確認のうえ、クチコミ一覧ページへ掲載させていただきます。</p>
-        <p class="com-mt-s com-fz-12"><span class="form-icon-required">必須</span>が付いている項目は必須入力です</p>
-        <?= do_shortcode('[contact-form-7 id="277" title="クチコミフォーム"]'); ?>
-    </div>
+	<div class="uk-container">
+		<p>下記フォームに必要項目を入力し送信ボタンを押してください。<br>内容を確認のうえ、クチコミ一覧ページへ掲載させていただきます。</p>
+		<p class="com-mt-s com-fz-12"><span class="form-icon-required">必須</span>が付いている項目は必須入力です</p>
+		<?= do_shortcode('[contact-form-7 id="277" title="クチコミフォーム"]'); ?>
+	</div>
 </section>
